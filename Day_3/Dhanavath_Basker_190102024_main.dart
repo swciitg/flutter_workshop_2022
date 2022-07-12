1501669aff16df4605639c7b58cf7fc245d39114
basker024 <87684206+basker024@users.noreply.github.com>
2022-07-12 12:10:23 +0530

    Create Dhanavath_Basker_190102024_main.dart

---
+++ Day_3/Dhanavath_Basker_190102024_main.dart
@@ -0,0 +1,406 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children:  [
+                    const Icon(Icons.menu),
+                    Row(
+                      children: const [
+                        Text("California "),
+                        Icon(Icons.keyboard_arrow_down),
+                      ],
+                    ),
+                    const Icon(Icons.search),
+                  ],
+                ),
+              ),
+              Container(
+                height: 100,
+                margin: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                //padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: const BorderRadius.all(Radius.circular(30)),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+
+                    const Expanded(
+                      flex: 1,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(vertical: 8),
+
+                        child: CircleAvatar(
+                          radius: 35,
+                          backgroundColor: Colors.yellow,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      flex: 3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children:  [
+                          Row(
+                            children: [
+                              Text("Dhanavath Basker  ",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 25,
+                                      color: Colors.indigo[900],
+                                  )
+                              ),
+                              const Icon(Icons.mode_edit_outlined),
+                            ],
+                          ),
+                          Row(
+                            children: const [
+                              Text("Aka Flutter developer  .  ",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text("22 friends",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: TextDecoration.underline,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 15),
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    child: Row(
+                      children: const [
+                        Text("Hello, ",
+                            style: TextStyle(
+                              fontSize: 30,
+                            ),
+
+                        ),
+                        Text("Dhanavath!",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 60,
+                        margin: const EdgeInsets.only(right: 8),
+                        decoration: const BoxDecoration(
+                          color: Color(0xffff5768),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                        child: Column(
+                          //mainAxisAlignment: MainAxisAlignment.,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 3, right: 10),
+                                  child: Icon(
+                                      color: Colors.white70,
+                                      Icons.business_center_outlined),
+                                ),
+
+                              ],
+                            ),
+                            const Text("  Professional",
+                                 style: TextStyle(
+                                   color: Colors.white70,
+                                   fontWeight: FontWeight.bold,
+                                 ),
+                              ),
+                            const Text("  events",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 60,
+                        margin: const EdgeInsets.only(left: 8),
+                        decoration: const BoxDecoration(
+                          color: Color(0xff3ad7e6),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 3, right: 10),
+                                  child: Icon(
+                                      color: Colors.white70,
+                                      Icons.volume_up_outlined),
+                                ),
+                              ],
+                            ),
+                            const Text("  Social",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Text("  events",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 60,
+                        margin: const EdgeInsets.only(right: 8),
+                        decoration: const BoxDecoration(
+                          color: Color(0xff4e53e2),
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: const [
+                                Padding(
+                                  padding: EdgeInsets.only(top: 3, right: 10),
+                                  child: Icon(
+                                      color: Colors.white70,
+                                      Icons.theater_comedy_outlined),
+                                ),
+                              ],
+                            ),
+                            const Text("  Concerts &",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const Text("  Theater",
+                              style: TextStyle(
+                                color: Colors.white70,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: Container(
+                          height: 60,
+                          margin: const EdgeInsets.only(left: 8),
+                          decoration: const BoxDecoration(
+                            color: Color(0xffff8e32),
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: const [
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 3, right: 10),
+                                    child: Icon(
+                                        color: Colors.white70,
+                                        Icons.people_outline_outlined),
+                                  ),
+                                ],
+                              ),
+                              const Text("  Events with",
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontWeight: FontWeight.bold,
+                                ),
+
+                              ),
+                              const Text("  friends",
+                                style: TextStyle(
+                                  color: Colors.white70,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const [
+                    Icon(Icons.brightness_1),
+                    Text("Most popular", style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                    Text("Friends go",style: TextStyle(fontSize: 18)),
+                    Text("Latest",style: TextStyle(fontSize: 18)),
+                    Text("Large ",style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+              ),
+              Container(
+                height: 210,
+                margin: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                decoration: const BoxDecoration(
+                  color: Color(0xff4d54e2),
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15,top: 15),
+                      child: Row(
+                        children: const [
+                          Text("Scorpion",
+                            style: TextStyle(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15,top: 5),
+                      child: Row(
+                        children: const [
+                          Text("World Tour - ANGEL TOUR",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15,top: 50),
+                      child: Row(
+                        children: const [
+                          Text("Data  ",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Text("12-01-2022 10AM",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15,top: 5),
+                      child: Row(
+                        children: const [
+                          Text("Location  ",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Text("PALACE ",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white70,
+                            ),
+                          ),
+                          Text("stadium",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white70,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
